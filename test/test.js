958a6ab993d86ee6f29dd69d14be083059c264c8
Tatas Fachrul <tatasfachrul@gmail.com>
2019-09-09 14:56:53 +0700

    edit

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const App = require('../app')
 const should = require('should')
-// const expect = require('chai').expect
+const expect = require('chai').expect
 const supertest = require('supertest')
 
 const server = supertest(App.server)
@@ -12,11 +12,6 @@ const admin = {
 
 let userToken, userId
 
-const user = {
-  email: 'user@user.com',
-  password: 'useruser'
-}
-
 describe('User Authentication', () => {
 
   it('User can login to get Token', (done) => {
@@ -25,7 +20,6 @@ describe('User Authentication', () => {
       .expect('Content-Type', /json/)
       .set('Authorization', 'X-CONTROL-APP')
       .send({ ...admin })
-      .expect(200)
       .end((err, res) => {
         res.status.should.equal(200)
         userToken = 'Bearer ' + res.body.result.token
@@ -43,7 +37,6 @@ describe('User can get data', () => {
       .set('Authorization', 'X-CONTROL-APP')
       .set('x-control-user', userId)
       .set('x-access-token', userToken)
-      .expect(200)
       .end((err, res) => {
         res.status.should.equal(200)
         res.body.should.have.property('result').which.is.a.Array()
