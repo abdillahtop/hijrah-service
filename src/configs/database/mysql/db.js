ed043951eae9fd120b98ab609e537fd97b2b11c9
abdillahtop <abdillahtop@gmail.com>
2020-02-11 20:12:00 +0700

    commit

--- src/configs/database/mysql/db.js
+++ src/configs/database/mysql/db.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: `${process.env.DB_PASSWORD}`,
   database: process.env.DB_NAME,
-  timezone: 'utc+07',
+  timezone: 'utc',
   charset: 'utf8mb4'
 })
 
