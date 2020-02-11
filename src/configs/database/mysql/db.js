05e1e1a8b4ee34d33c8df6091e4ba25475f87fd9
abdillahtop <abdillahtop@gmail.com>
2020-02-11 19:34:08 +0700

    charset mysql

--- src/configs/database/mysql/db.js
+++ src/configs/database/mysql/db.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: `${process.env.DB_PASSWORD}`,
   database: process.env.DB_NAME,
-  timezone: 'Asia/Jakarta'
+  timezone: 'Asia/Jakarta',
+  charset: 'utf8mb4'
 })
 
 connection.connect((err) => {
