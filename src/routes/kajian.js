f7509bfd22643953498704b940541d8226fa88ca
abdillahtop <abdillahtop@gmail.com>
2020-02-13 19:54:46 +0700

    bug add kajian

--- src/routes/kajian.js
+++ src/routes/kajian.js
@@ -25,7 +25,7 @@ Route
   .get('/kajian-organized', Auth.accesstoken, KajianController.getKajianbyOrganized)
   .get('/find-kajian', Auth.authInfoGlobal, KajianController.findKajian)
   .get('/detail-kajian/:kajianId', Auth.accesstokenNoMandatory, KajianController.detailKajian)
-  .post('/add-kajian', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'image-ustadz', maxCount: 8 }]), Auth.accesstoken, KajianController.addKajian)
+  .post('/add-kajian', upload.single('image'), Auth.accesstoken, KajianController.addKajian)
   .post('/add-member-kajian/:kajianId', Auth.accesstoken, KajianController.addMemberKajian)
   .patch('/update-kajian/:kajianId', upload.single('image'), Auth.accesstoken, KajianController.editKajian)
   .delete('/unjoin-kajian', Auth.accesstoken, KajianController.unjoinKajian)
